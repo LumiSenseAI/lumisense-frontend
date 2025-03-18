302d4d9f03413010bb5c4f446ebf760d38e6d0fd
Meldryck SAID <meldryck.d.said@gmail.com>
2025-03-18 16:00:51 +0100

    Custom Tableau

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,22 @@
 <template>
-    <div>
-      <h1>Bienvenue sur mon projet Vue.js 🚀</h1>
-      <router-view></router-view> <!-- Ici s'affichera le contenu des pages -->
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: "App"
-  }
-  </script>
-  
-  <style>
-  h1 {
-    text-align: center;
-    color: #42b983;
-  }
-  </style>
-  
+  <div>
+    <router-view></router-view>
+    <!-- Ici s'affichera le contenu des pages -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+body {
+  background-color: #303030;
+}
+h1 {
+  text-align: center;
+  color: #42b983;
+}
+</style>
